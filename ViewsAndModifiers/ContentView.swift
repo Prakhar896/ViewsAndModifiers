fafda1610fb272a85517ce58b93fadd52cb4deb2
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-17 23:15:28 +0800

    finished tutorial 4

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -12,22 +12,29 @@ struct ContentView: View {
         //        Text("Hello, world!")
         //            .frame(maxWidth: .infinity, maxHeight: .infinity)
         //            .background(.red)
-//        Button("Hello, world!") {
-//            print(type(of: self.body))
-//        }
-//        //        .background(.red)
-//        //        .frame(width: 200, height: 200)
-//        .frame(width: 200, height: 200)
-//        .background(.red)
-//        Text("Hello world")
-//            .padding()
-//            .background(.red)
-//            .padding()
-//            .background(.blue)
-//            .padding()
-//            .background(.green)
-//            .padding()
-//            .background(.yellow)
+        
+        //        Button("Hello, world!") {
+        //            print(type(of: self.body))
+        //        }
+        //        //        .background(.red)
+        //        //        .frame(width: 200, height: 200)
+        //        .frame(width: 200, height: 200)
+        //        .background(.red)
+        
+        //        Text("Hello world")
+        //            .padding()
+        //            .background(.red)
+        //            .padding()
+        //            .background(.blue)
+        //            .padding()
+        //            .background(.green)
+        //            .padding()
+        //            .background(.yellow)
+        
+        Text("Hello")
+        Text("World")
+        Text("Goodbye")
+        Text("World")
     }
 }
 
