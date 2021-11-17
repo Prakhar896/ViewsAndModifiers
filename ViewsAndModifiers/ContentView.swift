4ec6580cae234992dcb5b819ac889bda2c300e57
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-17 23:32:40 +0800

    finished tutorial 7

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,7 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var useRedText = false
+//    @State private var useRedText = false
+//    let motto1 = Text("Draco dormiens")
+    var motto1: some View {
+        Text("Draco dormiens")
+    }
+    var motto2 = Text("nunquam titillandus")
+    
+//    var spells: some View {
+//        Group {
+//            Text("Lumos")
+//            Text("Obliviate")
+//        }
+//    }
+    
+    @ViewBuilder var spells: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
     
     var body: some View {
         //        Text("Hello, world!")
@@ -43,16 +60,26 @@ struct ContentView: View {
 //        }
 //        .foregroundColor(useRedText ? .red: .blue)
         
-        VStack {
-            Text("Gryffindor")
-                .font(.largeTitle) //  the child's modifier is prioritised
-                .blur(radius: 0) // this will not work as blur is a regular modifier
-            Text("Hufflepuff")
-            Text("Ravenclaw")
-            Text("Slytherin")
-        }
-        .font(.title) // this is called an environment modifier
-        .blur(radius: 5) // regular modifiers are separate from environment modifiers
+//        VStack {
+//            Text("Gryffindor")
+//                .font(.largeTitle) //  the child's modifier is prioritised
+//                .blur(radius: 0) // this will not work as blur is a regular modifier
+//            Text("Hufflepuff")
+//            Text("Ravenclaw")
+//            Text("Slytherin")
+//        }
+//        .font(.title) // this is called an environment modifier
+//        .blur(radius: 5) // regular modifiers are separate from environment modifiers
+        
+//        VStack {
+////            motto1
+////                .foregroundColor(.red)
+////            motto2
+////                .foregroundColor(.blue)
+//            spells
+//        }
+        
+        spells
     }
 }
 
