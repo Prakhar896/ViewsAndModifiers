69fdfd8a3fb9578c7cd17777ca1b062b011c1e06
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-17 23:03:51 +0800

    finished tutorial 3

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,9 +9,25 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(.red)
+        //        Text("Hello, world!")
+        //            .frame(maxWidth: .infinity, maxHeight: .infinity)
+        //            .background(.red)
+//        Button("Hello, world!") {
+//            print(type(of: self.body))
+//        }
+//        //        .background(.red)
+//        //        .frame(width: 200, height: 200)
+//        .frame(width: 200, height: 200)
+//        .background(.red)
+//        Text("Hello world")
+//            .padding()
+//            .background(.red)
+//            .padding()
+//            .background(.blue)
+//            .padding()
+//            .background(.green)
+//            .padding()
+//            .background(.yellow)
     }
 }
 
