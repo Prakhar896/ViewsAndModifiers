291ed76b3961e488a5862b33b18253795220fc9d
Prakhar Trivedi <prakh0706@outlook.com>
2021-11-19 00:05:10 +0800

    finished challenges for project 3

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -7,23 +7,37 @@
 
 import SwiftUI
 
-struct GridStack<Content: View>: View {
-    let rows: Int
-    let columns: Int
-    @ViewBuilder let content: (Int, Int) -> Content
-    
-    var body: some View {
-        VStack {
-            ForEach(0..<rows, id: \.self) { row in
-                HStack {
-                    ForEach(0..<columns, id: \.self) { column in
-                        content(row, column)
-                    }
-                }
-            }
-        }
+struct NiceTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .foregroundColor(.blue)
     }
 }
+
+extension View {
+    func niceTitle() -> some View {
+        modifier(NiceTitle())
+    }
+}
+
+//struct GridStack<Content: View>: View {
+//    let rows: Int
+//    let columns: Int
+//    @ViewBuilder let content: (Int, Int) -> Content
+//
+//    var body: some View {
+//        VStack {
+//            ForEach(0..<rows, id: \.self) { row in
+//                HStack {
+//                    ForEach(0..<columns, id: \.self) { column in
+//                        content(row, column)
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
 
 //struct Watermark: ViewModifier {
 //    var text: String
@@ -98,10 +112,12 @@ struct ContentView: View {
     //    }
     
     var body: some View {
-        GridStack(rows: 4, columns: 4) { row, col in
-            Image(systemName: "\(row * 4 + col).circle")
-            Text("R\(row) C\(col)")
-        }
+//        GridStack(rows: 4, columns: 4) { row, col in
+//            Image(systemName: "\(row * 4 + col).circle")
+//            Text("R\(row) C\(col)")
+//        }
+        Text("Hello world")
+            .niceTitle()
         //        VStack {
         //            Text("Hello world")
         //    //            .modifier(Title())
